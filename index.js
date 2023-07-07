dcf3feedc232168880a17f6bd7b41cfba1c169a8
hokageCodes <hashsenju94@gmail.com>
2023-07-07 23:01:39 +0100

    Keppler done

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { log } = require('console');
-const { parse } = require('csv-parse'); 
-const fs = require("fs")
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
-    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
-}
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+    }
 
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
@@ -14,14 +15,16 @@ fs.createReadStream('kepler_data.csv')
         columns: true,
     }))
     .on('data', (data) => {
-        if(isHabitablePlanet(data)) {
-            habitablePlanets.push(data); 
+        if (isHabitablePlanet(data)) {
+        habitablePlanets.push(data);
         }
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(`${habitablePlanets.length} habtable planets found!`);
-    })
-// parse();
+        console.log(habitablePlanets.map((planet) => {
+        return planet['kepler_name'];
+        }));
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+    });
